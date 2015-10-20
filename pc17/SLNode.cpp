1513ed02147559822b90b02e5bb22061242ea64b
svenzor84 <steve.rossbyers@gmail.com>
2015-10-20 21:53:21 +0000

    pc17 complete

---
+++ pc17/SLNode.cpp
@@ -0,0 +1,42 @@
+/*
+ * Programming Challenge 17 SLNode Implementation
+ *
+ * Steve Ross-Byers
+ * Date created: 10/20/2015
+ * Last date modified: 10/20/2015
+ */
+ 
+ #include <stddef.h>
+ 
+ #include "SLNode.h"
+ 
+ SLNode::SLNode() : nextNode(NULL), contents(0) {
+     
+ }
+ 
+ SLNode::SLNode(int customContents) : nextNode(NULL), contents(customContents){
+     
+ }
+ 
+ SLNode::~SLNode(){
+     if (nextNode != NULL){
+         delete nextNode;
+         nextNode = NULL;
+     }
+ }
+ 
+ void SLNode::setContents(int newContents){
+     contents = newContents;
+ }
+ 
+ int SLNode::getContents() const{
+     return contents;
+ }
+ 
+ void SLNode::setNextNode(SLNode* nextSLNode){
+     nextNode = nextSLNode;
+ }
+ 
+ SLNode* SLNode::getNextNode () const{
+     return nextNode;
+ }
---
+++ pc17/SLNode.h
@@ -0,0 +1,68 @@
+/*
+ * Programming Challenge 17 SLNode Header
+ *
+ * Steve Ross-Byers
+ * Date created: 10/20/2015
+ * Last date modified: 10/20/2015
+ */
+
+#pragma once
+
+using namespace std;
+
+class SLNode {
+    
+    private:
+    
+        /*
+         * @member nextNode points to the next node in a singly-linked list
+         */
+        SLNode* nextNode;
+        
+        /*
+         * @member contents stores the data contents of this SLNode
+         */
+        int contents;
+        
+    public:
+    
+        /*
+         * SLNode default Constructor
+         */
+        SLNode();
+        
+        /*
+         * Overloaded SLNode Constructor
+         * @param customContents assigns this value to member contents
+         */
+        SLNode(int customContents);
+        
+        /*
+         * SLNode Destructor frees memory assigned to nextNode pointer
+         */
+        ~SLNode();
+        
+        /*
+         * Contents setter
+         * @param newContents assigns this value to member contents
+         */
+        void setContents(int newContents);
+        
+        /*
+         * Contents getter
+         * @return an int containing the data from this SLNode
+         */
+        int getContents() const;
+        
+        /*
+         * nextNode setter
+         * @param nextSLNode points nextNode at an SLNode
+         */
+        void setNextNode(SLNode* nextSLNode);
+        
+        /*
+         * nextNode getter
+         * @return an SLNode* pointing at the next node
+         */
+         SLNode* getNextNode ()const;
+};
